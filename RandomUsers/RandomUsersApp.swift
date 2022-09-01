015c7e12361e00e2121fcb6d2b859bc15992e87c
PilarCuAr <tauro10cuen@gmail.com>
2022-09-01 17:07:34 -0500

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by MAC on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
